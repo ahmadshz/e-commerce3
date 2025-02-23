616c53a4fb0ede317a55b877d411e91d871fea23
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-02-23 10:26:53 +0200

    fix size

--- src/Components/Websites/page/Intermadiate.jsx
+++ src/Components/Websites/page/Intermadiate.jsx
@@ -44,16 +44,16 @@ const Intermediate = () => {
     return (
         <div className="min-h-screen flex justify-center md:items-center container bg-white p-6 rounded-10px">
             <div className="container h-auto bg-background py-[20px]">
-                <h1 className="text-[20px] lg:text-[25px] text-primary font-semibold my-[10px] xl:my-5 ">
+                <h1 className="text-[20px] lg:text-[25px] text-primary font-bold my-[10px] xl:my-5 ">
                     بإضافة إعلانك على موقع دلال، فإنك توافق على الالتزام بشروط هذه الاتفاقية :
                 </h1>
                 <div className="grid lg:grid-cols-2 my-[10px] xl:my-5 gap-5 xl:gap-8">
                     {rules.map((rule, index) => (
-                        <div key={index} className="my-[10px] xl:my-5 font-semibold">
-                            <h2 className="text-[16px] lg:text-[20px] text-primary mb-4">{rule.title}</h2>
+                        <div key={index} className="my-[10px] xl:my-5 ">
+                            <h2 className="text-[16px] lg:text-[20px] font-semibold text-primary mb-4">{rule.title}</h2>
                             <ul className="list-disc list-inside space-y-3">
                                 {rule.items.map((item, itemIndex) => (
-                                    <li key={itemIndex} className=" text-[13px] lg:text-[17px]">
+                                    <li key={itemIndex} className=" font-normal text-[13px] lg:text-[17px]">
                                         {item}
                                     </li>
                                 ))}
@@ -61,16 +61,16 @@ const Intermediate = () => {
                         </div>
                     ))}
                 </div>
-                <h1 className="text-placeholder text-[16px] lg:text-[20px] font-semibold my-[10px] xl:my-5 ">
+                <h1 className="text-placeholder text-[16px] lg:text-[20px] font-bold my-[10px] xl:my-5 ">
                     القانون المعمول به: تخضع هذه الاتفاقية لقوانين الجمهورية العربية السورية.
                 </h1>
                 <div className="my-[10px] xl:my-5 ">
-                    <h1 className="text-primary text-[16px] lg:text-[20px]">رسوم الأعلان هي صفر ليرة سورية,</h1>
-                    <h1 className="text-primary text-[16px] lg:text-[20px]">يداّ بيد نعمر سوريا المستقبل</h1>
+                    <h1 className="text-primary text-[16px] lg:text-[20px] font-semibold">رسوم الأعلان هي صفر ليرة سورية,</h1>
+                    <h1 className="text-primary text-[16px] lg:text-[20px] font-semibold">يداّ بيد نعمر سوريا المستقبل</h1>
                 </div>
                 <div
                     onClick={handleAccept}
-                    className="bg-primary flex justify-center items-center rounded-10px h-[60px] md:h-[76px]  w-full md:w-[180px] xl:w-[211px] text-white text-[15px] md:text-[17px] xl:text-[20px] xl:my-5  cursor-pointer"
+                    className="bg-primary flex justify-center items-center rounded-10px h-[60px] md:h-[76px]  w-full md:w-[180px] xl:w-[211px] text-white text-[20px] lg:text-[25px] font-bold xl:my-5  cursor-pointer"
                 >
                     موافق
                 </div>
