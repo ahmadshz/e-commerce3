bcffcb85882bdecf4a7e5365c91b89c1fa8888de
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-02-23 10:54:45 +0200

    fix size

--- src/Components/Websites/page/Posts.jsx
+++ src/Components/Websites/page/Posts.jsx
@@ -15,19 +15,19 @@ const Posts = () => {
             <div className=' w-2/4 xl:w-2/5  flex '>
                 <div className='w-3/5 md:py-2  flex flex-col md:justify-between text-placeholder  text-[10px] md:text-[13px] lg:text-[17px]'>
                     <div className='flex  gap-1 md:gap-2 items-center'>
-                        <img className='w-4 md:w-5 lg:w-6' src={price} alt='' />
+                        <img className='w-3 md:w-5 lg:w-6' src={price} alt='' />
                         <div className='py-0' >20,000</div>
                     </div>
                     <div className='flex gap-1 md:gap-2 items-center'>
-                        <img className='w-4 md:w-5 lg:w-6' src={pricesy} alt='' />
+                        <img className='w-3 md:w-5 lg:w-6' src={pricesy} alt='' />
                         <div className='py-0' >260,040,000</div>
                     </div>
                     <div className='flex gap-1 md:gap-2 items-center'>
-                        <img className='w-4 md:w-5 lg:w-6' src={clock} alt='' />
+                        <img className='w-3 md:w-5 lg:w-6' src={clock} alt='' />
                         <div className='py-0' >قبل ساعة</div>
                     </div>
                     <div className='flex gap-1 md:gap-2 items-center'>
-                        <img className='w-4 md:w-5 lg:w-6' src={person} alt='' />
+                        <img className='w-3 md:w-5 lg:w-6' src={person} alt='' />
                         <div className='py-0'>أبو خالد</div>
                     </div>
                 </div>
