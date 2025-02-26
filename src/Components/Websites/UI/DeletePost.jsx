155e384380ea6a7eecee56d58faa8a5a9158194b
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-02-26 14:53:11 +0200

    add delete my post

--- src/Components/Websites/UI/DeletePost.jsx
+++ src/Components/Websites/UI/DeletePost.jsx
@@ -3,19 +3,20 @@ import React from 'react';
 const DeletePost = ({ closeDelete, deletePost }) => {
     return (
         <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
-            <div className="h-[313px] w-[700px] bg-[#F9F9F9] rounded-lg border-2 border-primary flex flex-col justify-evenly p-[20px]">
-                <h1 className="text-[25px] font-bold text-primary">مسح الاعلان</h1>
-                <h2 className="text-[20px] font-semibold">هل أنت متأكد أنك تريد مسح الاعلان ؟</h2>
-                <div className="flex w-full justify-between gap-4">
-                    <div
-                        onClick={deletePost} // Call deletePost when "نعم" is clicked
-                        className="bg-primary text-white w-[220px] h-[70px] text-[25px] font-bold flex items-center justify-center rounded-lg cursor-pointer"
-                    >
+            <div className="bg-[#F9F9F9] rounded-10px border-2 border-primary p-[20px] w-[85%] md:w-[500px] h-[313px]  flex flex-col justify-evenly shadow-lg">
+                <h1 className="text-[20px] md:text-[25px] font-bold text-primary text-center  md:text-start">
+                    مسح الاعلان
+                </h1>
+                <h2 className="text-[16px] md:text-[20px] font-semibold text-center md:text-start">
+                    هل أنت متأكد أنك تريد مسح الاعلان ؟
+                </h2>
+                <div className="flex flex-col md:flex-row w-full justify-between gap-4">
+                    <div onClick={deletePost} className="bg-primary text-white w-full md:w-[220px] h-[50px] md:h-[60px] xl:h-[70px] text-[16px] md:text-[25px] font-bold flex items-center justify-center rounded-10px">
                         نعم
                     </div>
                     <div
-                        onClick={closeDelete} // Close the modal when "لا" is clicked
-                        className="bg-[#009C46] text-white w-[220px] h-[70px] text-[25px] font-bold flex items-center justify-center rounded-lg cursor-pointer"
+                        onClick={closeDelete}
+                        className="bg-[#009C46] text-white w-full md:w-[220px] h-[50px] md:h-[60px] xl:h-[70px] text-[16px] md:text-[25px] font-bold flex items-center justify-center rounded-10px"
                     >
                         لا
                     </div>
--- src/Components/Websites/page/SinglePost.jsx
+++ src/Components/Websites/page/SinglePost.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import MainHeader from '../Header/MainHeader'
 
 const SinglePost = () => {
-  return (
-    <div>
-      <MainHeader />
-      <div>
-      
-      </div>
-    </div>
-  )
+    return (
+        <div className='flex flex-col gap-[100px]'>
+            <MainHeader />
+            <div className="min-h-screen container flex gap-[10px] md:gap-5 xl:gap-7">
+                <div className=" w-full  md:w-[1138px] ">
+                    <div className='bg-background w-full h-[155px] flex'>
+                        <div className='w-3/5 md:py-2 pr-2 md:pr-[10px] xl:pr-[20px] flex flex-col justify-between'>
+                            <div className='text-[15px] md:text-[19px] lg:text-[24px] font-semibold'>هوندا اكورد 2015 ستاندر </div>
+                            <div className='text-[13px] lg:text-[15px] font-normal text-placeholder'>دمشق</div>
+                        </div>
+                        <div className='w-1/5'>
+                        
+                        </div>
+                        <div className='w-1/5'>sdffs</div>
+                    </div>
+                </div>
+                <div className="hidden lg:block bg-bgsecondary w-[455px] "></div>
+            </div>
+
+        </div>
+    )
 }
 
 export default SinglePost
