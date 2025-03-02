fa60fe7fa056bd2b99e245f4252511fb27c73f22
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-03-02 10:19:25 +0200

    update

--- src/Components/Websites/UI/BarMobile.jsx
+++ src/Components/Websites/UI/BarMobile.jsx
@@ -3,30 +3,31 @@ import { NavLink } from 'react-router-dom';
 import home from '../../../assets/Carandothers/houses.svg'
 import { BiAddToQueue, BiCategory } from 'react-icons/bi';
 import { MdOutlineAccountCircle } from 'react-icons/md';
+import { FiPlus } from 'react-icons/fi';
 
 const BarMobile = () => {
     return (
-        <div className='fixed md:hidden z-[50] bottom-0 right-0 h-[80px] w-full bg-background'>
-            <div className='flex justify-around items-center h-full gap-2 text-primary font-bold text-[15px]'>
+        <div className='fixed md:hidden z-[50] bottom-0 right-0 h-[70px] w-full bg-background'>
+            <div className='flex justify-around items-center h-full gap-2 text-primary font-bold text-[13px]'>
                 <NavLink
                     className={({ isActive }) =>
                         `flex flex-col items-center ${isActive ? 'text-primary' : 'text-black'}`
                     }
                     to={'/'}
                 >
-                    <img className='w-8' src={home} alt='' />
+                    <img className='w-7' src={home} alt='' />
                     <span>الرئيسية</span>
                 </NavLink>
                 <NavLink className='text-primary flex flex-col items-center' to={'/'}>
-                    <BiCategory size={30} />
+                    <BiCategory size={25} />
                     <span>الاقسام</span>
                 </NavLink>
                 <NavLink className='text-primary flex flex-col items-center' to={'/'}>
-                    <BiAddToQueue size={30} />
-                    <span>اضافة اعلان</span>
+                <FiPlus size={25} />               
+                     <span>اضافة اعلان</span>
                 </NavLink>
                 <NavLink className='text-primary flex flex-col items-center' to={'/'}>
-                    <MdOutlineAccountCircle size={30} />
+                    <MdOutlineAccountCircle size={25} />
                     <span>حسابي</span>
                 </NavLink>
 
