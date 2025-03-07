d6516338d7b3001e3f37177f6c60c25735904875
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-03-07 23:57:28 +0200

    update

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -110,20 +110,20 @@ const Register = () => {
     return (
         <div className='flex flex-col gap-4'>
             <Navbar />
-            <h1 className='text-[25px] lg:text-[30px] w-full text-primary font-bold my-5 md:my-10 lg:my-14  container'>
+            <h1 className='text-[16px] md:text-[20px] lg:text-[25px] w-full text-primary font-bold my-4 md:my-10 lg:my-14  container'>
                 إنشاء حساب جديد
             </h1>
             <div className=' container flex flex-col  '>
-                <form className='grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8  mb-10 sm:mb-16 lg:mb-20' onSubmit={handleSubmit}>
+                <form className='grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-6 md:gap-8  mb-10 sm:mb-16 lg:mb-20' onSubmit={handleSubmit}>
                     {/* First Name */}
                     <div className='flex flex-col lg:flex-row   md:gap-4'>
-                        <label htmlFor="firstName" className="font-semibold text-[17px] lg:text-[20px] md:w-[239px] flex items-center">الاسم</label>
+                        <label htmlFor="firstName" className="  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  md:w-[239px] flex items-center">الاسم</label>
                         <input
                             id='firstName'
                             type='text'
                             value={formData.firstName}
                             onChange={handleChange}
-                            className='text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary'
+                            className='text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary'
                             placeholder='اكتب اسمك باللغة العربية'
                             required
                         />
@@ -131,7 +131,7 @@ const Register = () => {
 
                     {/* Last Name */}
                     <div className="flex flex-col lg:flex-row   md:gap-4">
-                        <label htmlFor="lastName" className="font-semibold text-[17px] lg:text-[20px] md:w-[239px] flex items-center">
+                        <label htmlFor="lastName" className="  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  md:w-[239px] flex items-center">
                             اﺳﻢ اﻟﻌﺎﺋﻠﺔ
                         </label>
                         <input
@@ -139,7 +139,7 @@ const Register = () => {
                             type="text"
                             value={formData.lastName}
                             onChange={handleChange}
-                            className="text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
+                            className="text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
                             placeholder="اكتب اسم العائلة باللغة العربية"
                             required
                         />
@@ -147,7 +147,7 @@ const Register = () => {
 
                     {/* Username */}
                     <div className='flex flex-col lg:flex-row   md:gap-4'>
-                        <label htmlFor="username" className='font-semibold text-[17px] lg:text-[20px] md:w-[239px] flex items-center'>
+                        <label htmlFor="username" className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  md:w-[239px] flex items-center'>
                             اﺳﻢ اﻟﻤﺴﺘﺨﺪم
                         </label>
                         <div className="relative w-full lg:w-[433px]">
@@ -156,11 +156,11 @@ const Register = () => {
                                 type='text'
                                 value={formData.username}
                                 onChange={handleChange}
-                                className="text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border w-full h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider pl-10 outline-none focus:border-primary"
+                                className="text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border w-full h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider pl-10 outline-none focus:border-primary"
                                 placeholder='اكتب اسم المستخدم باللغة الانجليزية'
                                 required
                             />
-                            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-placeholder text-[15px] lg:text-[17px] pointer-events-none">
+                            <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] pointer-events-none">
                                 @
                             </span>
                         </div>
@@ -168,13 +168,13 @@ const Register = () => {
 
                     {/* Phone Number */}
                     <div className='flex flex-col lg:flex-row   md:gap-4'>
-                        <label htmlFor="phone" className='font-semibold text-[17px] lg:text-[20px] md:w-[239px] flex items-center'>رقم الهاتف</label>
+                        <label htmlFor="phone" className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  md:w-[239px] flex items-center'>رقم الهاتف</label>
                         <input
                             id='phone'
                             type='tel'
                             value={formData.phone}
                             onChange={handleChange}
-                            className="text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
+                            className="text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border w-full lg:w-[433px] h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
                             placeholder='+963 9.. ... ...'
                             required
                         />
@@ -182,13 +182,13 @@ const Register = () => {
 
                     {/* Email */}
                     <div className='flex flex-col lg:flex-row   md:gap-4 col-span-1 sm:col-span-2'>
-                        <label htmlFor="email" className='font-semibold text-[17px] lg:text-[20px] xl:w-[239px] flex items-center'>البريد الإلكتروني</label>
+                        <label htmlFor="email" className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  xl:w-[239px] flex items-center'>البريد الإلكتروني</label>
                         <input
                             id='email'
                             type='email'
                             value={formData.email}
                             onChange={handleChange}
-                            className='text-placeholder text-left border md:border-2 border-border w-full lg:w-[800px] xl:w-[1183px] 2xl:w-[1202px] h-[50px] md:h-[60px] lg:h-[76px] rounded-10px text-[15px] lg:text-[17px] font-medium tracking-wider p-3 flex items-center outline-none focus:border-primary'
+                            className='text-placeholder text-left border md:border-2 border-border w-full lg:w-[800px] xl:w-[1183px] 2xl:w-[1202px] h-[50px] md:h-[60px] lg:h-[76px] rounded-10px  text-[12px] md:text-[14px] lg:text-[17px] font-medium tracking-wider p-3 flex items-center outline-none focus:border-primary'
                             placeholder='@'
                             required
                         />
@@ -196,14 +196,14 @@ const Register = () => {
 
                     {/* Password */}
                     <div className='flex flex-col lg:flex-row   md:gap-4 col-span-1 sm:col-span-2'>
-                        <label htmlFor="password" className='font-semibold text-[17px] lg:text-[20px] lg:w-[142px] xl:w-[186px] 2xl:w-[239px] flex items-center'>كلمة المرور</label>
+                        <label htmlFor="password" className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  lg:w-[142px] xl:w-[186px] 2xl:w-[239px] flex items-center'>كلمة المرور</label>
                         <div className="relative w-full md:w-[350px] xl:w-[477px]">
                             <input
                                 id='password'
                                 type={showPassword ? 'text' : 'password'}
                                 value={formData.password}
                                 onChange={handleChange}
-                                className="text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border
+                                className="text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border
                                  w-full  h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px
                                   font-medium tracking-wider p-3 outline-none focus:border-primary"
                                 placeholder='اكتب كلمة السر باللغة الانجليزية'
@@ -222,14 +222,14 @@ const Register = () => {
 
                     {/* Confirm Password */}
                     <div className='flex flex-col lg:flex-row   md:gap-4 col-span-1 sm:col-span-2'>
-                        <label htmlFor="confirmPassword" className='font-semibold text-[17px] lg:text-[20px] lg:w-[142px] xl:w-[186px] 2xl:w-[239px] flex items-center'>تأكيد كلمة المرور</label>
+                        <label htmlFor="confirmPassword" className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  lg:w-[142px] xl:w-[186px] 2xl:w-[239px] flex items-center'>تأكيد كلمة المرور</label>
                         <div className="relative w-full md:w-[350px] xl:w-[477px]">
                             <input
                                 id='confirmPassword'
                                 type={showConfirmPassword ? 'text' : 'password'}
                                 value={formData.confirmPassword}
                                 onChange={handleChange}
-                                className="text-placeholder text-[15px] lg:text-[17px] border md:border-2 border-border w-full h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
+                                className="text-placeholder  text-[12px] md:text-[14px] lg:text-[17px] border md:border-2 border-border w-full h-[50px] md:h-[60px] lg:h-[76px] flex items-center rounded-10px font-medium tracking-wider p-3 outline-none focus:border-primary"
                                 placeholder='اكتب كلمة السر ثانياّ باللغة الانجليزية'
                                 required
                             />
@@ -238,20 +238,20 @@ const Register = () => {
                                 className="absolute text-placeholder inset-y-0 left-2 lg:left-5 flex items-center"
                                 onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                             >
-                            {showPassword ? <FaRegEyeSlash className='text-[25px] md:text-[30px]' /> : <BiShow className='text-[25px] md:text-[30px]'  />}
+                            {showConfirmPassword ? <FaRegEyeSlash className='text-[25px] md:text-[30px]' /> : <BiShow className='text-[25px] md:text-[30px]'  />}
                             </button>
                         </div>
                     </div>
 
                     {/* Error Message */}
-                    {error && <p className='font-semibold text-[17px] lg:text-[20px] text-primary col-span-1 md:col-span-2 text-center'>{error}</p>}
+                    {error && <p className='  text-[14px] md:text-[16px] lg:text-[20px] font-semibold  text-primary col-span-1 md:col-span-2 text-center'>{error}</p>}
 
                     {/* Submit Button */}
                     <div className='col-span-1 md:col-span-2 flex justify-center items-center my-4 md:my-10'>
                         <button
 
                             type="submit"
-                            className={`bg-primary font-bold text-lg md:text-[25px] text-white  h-[50px] md:h-[60px] lg:h-[76px] w-[660px] rounded-10px ${loading ? 'opacity-50' : ''}`}
+                            className={`bg-primary font-bold text-[16px] md:text-[20px] lg:text-[25px] text-white  h-[50px] md:h-[60px] lg:h-[76px] w-[660px] rounded-10px ${loading ? 'opacity-50' : ''}`}
                             disabled={loading ? true : false}
                         >
                             {loading ? 'جاري إنشاء الحساب...' : 'اﻧﺸﺎء ﺣﺴﺎب ﺟﺪﻳﺪ'}
