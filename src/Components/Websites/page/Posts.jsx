8e63610173107196d497c0c5929fe35fbf34b21b
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-03-07 17:27:56 +0200

    update

--- src/Components/Websites/page/Posts.jsx
+++ src/Components/Websites/page/Posts.jsx
@@ -87,7 +87,7 @@ const Posts = ({ ads, selectedCategory, selectedBrand, visibleCount, sponsorImag
                             initial={{ opacity: 0, y: 20 }}
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.5, delay: (totalDisplayedAds - visibleCount + index) * 0.2 }}
-                            className='bg-[#FAFAFA] w-full h-[100px] md:h-[140px] lg:h-[160px] flex justify-between'>
+                            className='bg-[#FAFAFA] w-full h-[120px] md:h-[140px] lg:h-[160px] flex justify-between'>
                             <div className='w-4/6 lg:w-4/5 px-3  md:p-3 h-full flex flex-col justify-between'>
                                 <Link to={`/singlePost/${item._id}`} className='text-[12px] lg:text-[20px] font-semibold 
                                 truncate w-full   '>{item.title}</Link>
